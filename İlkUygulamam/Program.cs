6cae55099772976e42ee8900753bcf031121e6e2
Ayatar <55198539+Ayatar@users.noreply.github.com>
2024-01-03 01:47:51 +0300

    Add files via upload

---
+++ İlkUygulamam/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IlkUygulamam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Birinci sayıyı giriniz: ");
+            double s1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("İkinci sayıyı giriniz: ");
+            double s2 = Convert.ToDouble(Console.ReadLine());
+
+            double sonuc = s1 + s2;
+            Console.Write("İşlemin sonucu: " + sonuc.ToString());          
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
